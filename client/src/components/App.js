6ac294cba074804d3f97c12d651d9bb7025b2224
satyajitn <satyajit.nalavade@gmail.com>
2022-09-10 16:31:51 -0500

    added authReducer and React router

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+
+
+const App = () => {
+    return (
+        <div>
+            hi there
+        </div>
+    );
+};
+
+export default App;
