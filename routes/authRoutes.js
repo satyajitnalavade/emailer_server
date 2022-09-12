b7377251b6f0dd22e96abbce35e9877dbd4b677b
satyajitn <satyajit.nalavade@gmail.com>
2022-09-12 00:28:37 -0500

    updated routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,6 @@ const passport = require('passport')
 
 module.exports = (app) => {
     
-    function isLoggedIn(req,res,next) {
-        req.user ? next() : res.sendStatus(401);
-    }
-
-
     app.get('/auth/google',
     passport.authenticate('google', { scope: ['profile','email'] }));
 
@@ -14,13 +9,8 @@ module.exports = (app) => {
     passport.authenticate('google', { scope: ['profile','email'], failureRedirect: '/auth/failure' }),
     function(req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/api/current_user');
+    res.redirect('/surveys');
     });
-
-    app.get('/', (req,res) => {
-        res.send('<a href="/auth/google">Authenticate with Google</a>');
-    });
-
 
     app.get('/auth/failure', (req,res) => {
         res.send('something went wrong.......');
